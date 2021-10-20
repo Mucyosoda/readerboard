cd1ef56cb9c83b934bb183824f8821b51c91aaba
Mucyosoda <mucyosofa@gmail.com>
2021-10-20 22:43:45 +0100

    add readme file

---
+++ src/list.js
@@ -0,0 +1,52 @@
+const listArr = [
+  {
+    Name: 'Mucyo',
+    score: 100,
+  },
+  {
+    Name: 'Claude',
+    score: 100,
+  },
+  {
+    Name: 'Jeannette',
+    score: 100,
+  },
+  {
+    Name: 'Musha',
+    score: 100,
+  },
+  {
+    Name: 'Musha',
+    score: 100,
+  },
+  {
+    Name: 'Musha',
+    score: 10,
+  },
+  {
+    Name: 'Musha',
+    score: 40,
+  },
+  {
+    Name: 'Musha',
+    score: 60,
+  },
+  {
+    Name: 'Musha',
+    score: 100,
+  },
+  {
+    Name: 'Musha',
+    score: 70,
+  },
+  {
+    Name: 'Musha',
+    score: 80,
+  },
+  {
+    Name: 'Musha',
+    score: 90,
+  },
+];
+
+export default listArr;
